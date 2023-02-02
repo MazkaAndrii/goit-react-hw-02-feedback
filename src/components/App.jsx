d86b6a4ff4c737988292e44c471a4df773cd90c5
MazkaAndrii <andriy.mazka@gmail.com>
2023-02-02 21:11:56 +0200

    add section title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+import Section from './Feedback/Section/Section';
 export const App = () => {
   return (
     <div
@@ -7,10 +9,25 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Feedback />
     </div>
   );
 };
+
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {
+    return (
+      <div>
+        <Section title="Please, leave feedback"></Section>
+      </div>
+    );
+  }
+}
